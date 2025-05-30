f622ecfe7e9c3afc6aff0ae06edda137be59cee8
Aitan Martinez <161392373+Tortuga-AM@users.noreply.github.com>
2025-05-30 02:37:42 +0000

    Emergency bug fix

--- BumperBias/src/SupabaseClient.jsx
+++ BumperBias/src/SupabaseClient.jsx
@@ -1,8 +1,8 @@
 // src/SupabaseClient.jsx
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = "https://iyvitvgcjmonbraatjvm.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5dml0dmdjam1vbmJyYWF0anZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzMTkzNDQsImV4cCI6MjA2Mzg5NTM0NH0.TtVob1P5Dlvnb9Hi-SnycETen-unEuTPpD_CKecFIkM";
 
 // Create a Supabase client instance
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
